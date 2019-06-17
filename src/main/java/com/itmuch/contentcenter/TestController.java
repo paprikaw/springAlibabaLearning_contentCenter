276b479486cda2f6b7e21230f34d2d4c78cd62b4
周立 <eacdy0000@126.com>
2019-06-18 07:35:36 +0800

    将应用注册到nacos上

--- src/main/java/com/itmuch/contentcenter/TestController.java
+++ src/main/java/com/itmuch/contentcenter/TestController.java
@@ -3,6 +3,8 @@ package com.itmuch.contentcenter;
 import com.itmuch.contentcenter.dao.content.ShareMapper;
 import com.itmuch.contentcenter.domain.entity.content.Share;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +15,8 @@ import java.util.List;
 public class TestController {
     @Autowired
     private ShareMapper shareMapper;
+    @Autowired
+    private DiscoveryClient discoveryClient;
     @GetMapping("/test")
     public List<Share> testInsert(){
         // 1. 做插入
@@ -30,5 +34,16 @@ public class TestController {
         List<Share> shares = this.shareMapper.selectAll();
 
         return shares;
+    }
+
+    /**
+     * 测试：服务发现，证明内容中心总能找到用户中心
+     * @return 用户中心所有实例的地址信息
+     */
+    @GetMapping("test2")
+    public List<ServiceInstance> getInstances(){
+        // 查询指定服务的所有实例的信息
+        // consul/eureka/zookeeper...
+        return this.discoveryClient.getInstances("user-center");
     }
 }
