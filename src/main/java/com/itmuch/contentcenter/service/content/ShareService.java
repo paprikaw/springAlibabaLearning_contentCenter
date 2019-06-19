d0d2349209a42526d5e5c3dc610b568df3e9fb24
周立 <eacdy0000@126.com>
2019-06-19 22:46:37 +0800

    05-06 为内容中心引入服务发现

--- src/main/java/com/itmuch/contentcenter/service/content/ShareService.java
+++ src/main/java/com/itmuch/contentcenter/service/content/ShareService.java
@@ -5,17 +5,24 @@ import com.itmuch.contentcenter.domain.dto.content.ShareDTO;
 import com.itmuch.contentcenter.domain.dto.user.UserDTO;
 import com.itmuch.contentcenter.domain.entity.content.Share;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.List;
+
+@Slf4j
 @Service
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class ShareService {
     private final ShareMapper shareMapper;
     private final RestTemplate restTemplate;
+    private final DiscoveryClient discoveryClient;
 
     public ShareDTO findById(Integer id) {
         // 获取分享详情
@@ -23,8 +30,21 @@ public class ShareService {
         // 发布人id
         Integer userId = share.getUserId();
 
+        // 强调：
+        // 了解stream --> JDK 8
+        // lambda表达式
+        // functional --> 函数式编程
+        // 用户中心所有实例的信息
+        List<ServiceInstance> instances = discoveryClient.getInstances("user-center");
+        String targetURL = instances.stream()
+            // 数据变换
+            .map(instance -> instance.getUri().toString() + "/users/{id}")
+            .findFirst()
+            .orElseThrow(() -> new IllegalArgumentException("当前没有实例！"));
+
+        log.info("请求的目标地址：{}", targetURL);
         UserDTO userDTO = this.restTemplate.getForObject(
-            "http://localhost:8080/users/{id}",
+            targetURL,
             UserDTO.class, userId
         );
 
