7cba385adec2f1f162f7395b23a20371a2f7cc55
周立 <eacdy0000@126.com>
2019-06-28 00:24:40 +0800

    06-08 细粒度配置自定义03-配置属性方式

--- src/main/java/com/itmuch/contentcenter/configuration/UserCenterRibbonConfiguration.java
+++ src/main/java/com/itmuch/contentcenter/configuration/UserCenterRibbonConfiguration.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.context.annotation.Configuration;
 import ribbonconfiguration.RibbonConfiguration;
 
-@Configuration
-@RibbonClient(name = "user-center", configuration = RibbonConfiguration.class)
+//@Configuration
+//@RibbonClient(name = "user-center", configuration = RibbonConfiguration.class)
 public class UserCenterRibbonConfiguration {
 }
