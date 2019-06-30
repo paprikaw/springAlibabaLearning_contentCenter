2b544a51e3726e3f268a1e42669c9113a6a676e5
周立 <eacdy0000@126.com>
2019-06-30 15:10:11 +0800

    ribbon全局配置

--- src/main/java/com/itmuch/contentcenter/configuration/UserCenterRibbonConfiguration.java
+++ src/main/java/com/itmuch/contentcenter/configuration/UserCenterRibbonConfiguration.java
@@ -1,10 +1,11 @@
 package com.itmuch.contentcenter.configuration;
 
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClients;
 import org.springframework.context.annotation.Configuration;
 import ribbonconfiguration.RibbonConfiguration;
 
-//@Configuration
-//@RibbonClient(name = "user-center", configuration = RibbonConfiguration.class)
+@Configuration
+@RibbonClients(defaultConfiguration = RibbonConfiguration.class)
 public class UserCenterRibbonConfiguration {
 }
