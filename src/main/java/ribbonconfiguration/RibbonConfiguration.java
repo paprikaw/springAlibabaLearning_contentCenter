ca58857e5c1e5309a19983495f9685ae30ede0bd
周立 <eacdy0000@126.com>
2019-06-30 18:45:59 +0800

    扩展Ribbon-支持Nacos权重

--- src/main/java/ribbonconfiguration/RibbonConfiguration.java
+++ src/main/java/ribbonconfiguration/RibbonConfiguration.java
@@ -1,7 +1,7 @@
 package ribbonconfiguration;
 
+import com.itmuch.contentcenter.configuration.NacosWeightedRule;
 import com.netflix.loadbalancer.IRule;
-import com.netflix.loadbalancer.RandomRule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,6 +9,6 @@ import org.springframework.context.annotation.Configuration;
 public class RibbonConfiguration {
     @Bean
     public IRule ribbonRule() {
-        return new RandomRule();
+        return new NacosWeightedRule();
     }
 }
