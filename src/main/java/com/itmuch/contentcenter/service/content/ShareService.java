519936fd00c1f738c4e2ccbed5725668b1f932dc
周立 <eacdy0000@126.com>
2019-06-30 23:48:23 +0800

    ribbon-现有架构存在的问题

--- src/main/java/com/itmuch/contentcenter/service/content/ShareService.java
+++ src/main/java/com/itmuch/contentcenter/service/content/ShareService.java
@@ -31,6 +31,10 @@ public class ShareService {
         // 发布人id
         Integer userId = share.getUserId();
 
+        // 1. 代码不可读
+        // 2. 复杂的url难以维护：https://user-center/s?ie={ie}&f={f}&rsv_bp=1&rsv_idx=1&tn=baidu&wd=a&rsv_pq=c86459bd002cfbaa&rsv_t=edb19hb%2BvO%2BTySu8dtmbl%2F9dCK%2FIgdyUX%2BxuFYuE0G08aHH5FkeP3n3BXxw&rqlang=cn&rsv_enter=1&rsv_sug3=1&rsv_sug2=0&inputT=611&rsv_sug4=611
+        // 3. 难以相应需求的变化，变化很没有幸福感
+        // 4. 编程体验不统一
         UserDTO userDTO = this.restTemplate.getForObject(
             "http://user-center/users/{userId}",
             UserDTO.class, userId
